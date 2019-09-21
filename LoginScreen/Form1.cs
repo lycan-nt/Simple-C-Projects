3cccb0cc03fd6c1c7ab564b3a21e8e54f99bf25f
Felipe D. Santos <lycan.felipe@gmail.com>
2019-09-21 20:43:53 -0300

    Added a new form and implemented the code in login validation to initialize form 2 and close the first one

--- LoginScreen/Form1.cs
+++ LoginScreen/Form1.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace LoginScreen
 {
     public partial class FrmLogin : Form
     {
+
+        Thread nt; 
+
+
         public FrmLogin()
         {
             InitializeComponent();
@@ -20,18 +25,37 @@ namespace LoginScreen
         private void Button1_Click(object sender, EventArgs e)
         {
 
+            //Validetes if the login and password entered is the same as login: Master
+            //and password 1, the returns a message and the close the login form and initializes the second form
             if ((TxbLogin.Text == "Master") && (TxbPassword.Text == "1") )
             {
 
                 MessageBox.Show("Welcome to the system!");
 
+                this.Close();
+
+                nt = new Thread(NewForm);
+                nt.SetApartmentState(ApartmentState.STA);
+                nt.Start();
+
+                
             }
+            //If the conditions is not met issues an erro message
             else
             {
 
                 MessageBox.Show("Invalid login or password.");
 
             }
+
+        
+
+        }
+
+        private void NewForm()
+        {
+
+            Application.Run(new Form2());
 
         }
     }

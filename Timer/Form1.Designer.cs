05cb4a102b8865538d5c78ca2ebc3d5785767891
Felipe D. Santos <lycan.felipe@gmail.com>
2019-09-22 12:59:05 -0300

    added to the project the timer component and implemented the start button functions

--- Timer/Form1.Designer.cs
+++ Timer/Form1.Designer.cs
@@ -28,21 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmTimer));
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TxbTime = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.LblTime = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // TxbTime
             // 
-            this.textBox1.Location = new System.Drawing.Point(120, 60);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(119, 20);
-            this.textBox1.TabIndex = 0;
+            this.TxbTime.Location = new System.Drawing.Point(120, 60);
+            this.TxbTime.Name = "TxbTime";
+            this.TxbTime.Size = new System.Drawing.Size(119, 20);
+            this.TxbTime.TabIndex = 0;
             // 
             // label1
             // 
@@ -54,15 +56,15 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Time";
             // 
-            // label2
+            // LblTime
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(275, 60);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 22);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "00:00";
+            this.LblTime.AutoSize = true;
+            this.LblTime.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTime.Location = new System.Drawing.Point(275, 60);
+            this.LblTime.Name = "LblTime";
+            this.LblTime.Size = new System.Drawing.Size(61, 22);
+            this.LblTime.TabIndex = 2;
+            this.LblTime.Text = "00:00";
             // 
             // button1
             // 
@@ -72,6 +74,7 @@
             this.button1.TabIndex = 3;
             this.button1.Text = "Start";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // pictureBox1
             // 
@@ -83,6 +86,11 @@
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
             // FrmTimer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -90,9 +98,9 @@
             this.ClientSize = new System.Drawing.Size(381, 180);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.LblTime);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TxbTime);
             this.Name = "FrmTimer";
             this.Text = "Timer";
             this.Load += new System.EventHandler(this.FrmTimer_Load);
@@ -104,11 +112,12 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TxbTime;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LblTime;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Timer/Form1.cs
+++ Timer/Form1.cs
@@ -12,6 +12,11 @@ namespace Timer
 {
     public partial class FrmTimer : Form
     {
+
+        int Time = 0;
+        int Minute = 0;
+        int Second = 0;
+
         public FrmTimer()
         {
             InitializeComponent();
@@ -21,6 +26,65 @@ namespace Timer
         {
 
             pictureBox1.Visible = false;
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            pictureBox1.Visible = false;
+
+            Time = Convert.ToInt16(TxbTime.Text);
+
+            if (Time >= 60)
+            {
+
+                Minute = Time / 60;
+                Second = Time % 60;
+
+            }
+            else
+            {
+
+                Minute = 0;
+                Second = Time;
+
+            }
+
+            LblTime.Text = "0" + Minute + ":" + Second;
+
+            timer1.Enabled = true;
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+
+            Second--;
+
+            if (Minute > 0)
+            {
+
+                if (Second < 0)
+                {
+
+                    Second = 59;
+                    Minute--;
+                }
+
+                LblTime.Text = "0" + Minute + ":" + Second;
+
+            }
+
+            Second--;
+
+            if (Minute == 0 && Second == 0)
+            {
+
+                timer1.Enabled = false;
+                pictureBox1.Visible = true;
+
+            }
 
         }
     }

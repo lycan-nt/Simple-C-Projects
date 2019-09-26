f97574ffbb684c97e8e5682863f134c95a9de59c
Felipe D. Santos <lycan.felipe@gmail.com>
2019-09-26 15:27:26 -0300

    Implemented the code identification variables and the method to load the values and added some images to the project.

--- Cashier/FrmCashier.cs
+++ Cashier/FrmCashier.cs
@@ -12,6 +12,12 @@ namespace Cashier
 {
     public partial class FrmCachier : Form
     {
+
+        string [] codProd   = new string[6];
+        string [] nameProd  = new string[6];
+        decimal[] valueProd = new decimal[6];
+        decimal   totSum;
+
         public FrmCachier()
         {
             InitializeComponent();
@@ -19,6 +25,37 @@ namespace Cashier
 
         private void FrmCachier_Load(object sender, EventArgs e)
         {
+
+            loadProducts();
+            totSum = 0;
+
+        }
+
+        //Method to load the products and start the variables
+        private void loadProducts ()
+        {
+
+            codProd[1] = "1";
+            codProd[2] = "2";
+            codProd[3] = "3";
+            codProd[4] = "4";
+            codProd[5] = "5";
+
+            nameProd[1] = "Coca-Cola";
+            nameProd[2] = "Chocolate";
+            nameProd[3] = "MotorCycle";
+            nameProd[4] = "Car";
+            nameProd[5] = "Katana";
+
+            valueProd[1] = 4.50M;
+            valueProd[2] = 2.20M;
+            valueProd[3] = 12.000M;
+            valueProd[4] = 20.000M;
+            valueProd[5] = 3.000M;
+
+
+
+        
 
         }
 
